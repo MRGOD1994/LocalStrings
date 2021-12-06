a593962b9c25ee2df43503f53d27013df5f41423
TengFei <shentengfei@esunny.cc>
2021-12-06 16:23:38 +0800

    项目添加

---
+++ LocalStrings/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  LocalStrings
+//
+//  Created by 沈腾飞 on 2021/12/6.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ LocalStrings/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LocalStrings
+//
+//  Created by 沈腾飞 on 2021/12/6.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
